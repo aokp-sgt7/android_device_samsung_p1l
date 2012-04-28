f27adaf7370d68568f87c298350aa13ef432e761
eaut <eaut@boehm.org>
2012-04-28 18:16:34 +0200

    add p1l cm makefile

---
+++ cm.mk
@@ -0,0 +1,22 @@
+#
+# CyanogenMod product configuration
+#
+
+# --------------------------------------------------------------------------------
+# Inherit CM stuff
+# --------------------------------------------------------------------------------
+
+$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# --------------------------------------------------------------------------------
+# Inherit device configuration
+# --------------------------------------------------------------------------------
+
+$(call inherit-product, device/samsung/p1/full_p1l.mk)
+
+# --------------------------------------------------------------------------------
+# override product name - the rest is already defined in full_p1.mk
+# --------------------------------------------------------------------------------
+
+PRODUCT_NAME := cm_p1l
