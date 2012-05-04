dda21d7e58596c1f589cc730341410ea0e937c43
eaut <eaut@boehm.org>
2012-05-04 23:53:02 +0200

    adjust to makefile simplification
    Change-Id: I14c946265b63939a697563b085726add24f2880a

--- cm.mk
+++ cm.mk
@@ -13,10 +13,23 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 # --------------------------------------------------------------------------------
 
-$(call inherit-product, device/samsung/p1/full_p1l.mk)
+$(call inherit-product, device/samsung/p1/p1.mk)
 
 # --------------------------------------------------------------------------------
-# override product name - the rest is already defined in full_p1l.mk
+# product specific settings
 # --------------------------------------------------------------------------------
+
+# ISDB-T support
+TARGET_INITRC_IMPORT += tv.rc
+PRODUCT_COPY_FILES += device/samsung/p1/tv.rc:root/tv.rc
 
 PRODUCT_NAME := cm_p1l
+PRODUCT_DEVICE := p1l
+PRODUCT_MODEL := GT-P1000L
+
+# kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/p1
+TARGET_KERNEL_CONFIG := cyanogenmod_$(PRODUCT_DEVICE)_defconfig
+
+# asserts
+TARGET_OTA_ASSERT_DEVICE := galaxytab,$(PRODUCT_MODEL)
